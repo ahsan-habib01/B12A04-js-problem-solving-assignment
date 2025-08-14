42dfa66e5afef4fe7a44af9a5758daefe82cf70e
ahsan.habib01 <ahshapon00@gmail.com>
2025-08-14 12:23:01 +0600

    completed the assignment 4

--- problem5.js
+++ problem5.js
@@ -3,6 +3,29 @@ function resultReport(marks) {
   if (!Array.isArray(marks)) {
     return 'Invalid';
   }
+
+  if (marks.length === 0) {
+    return { finalScore: 0, pass: 0, fail: 0 };
+  }
+
+  let total = 0;
+  let pass = 0;
+  let fail = 0;
+
+  for (let mark of marks) {
+    total += mark;
+
+    finalScore = Math.round(total / marks.length);
+
+    if (mark >= 40) {
+      pass++;
+    } else {
+      fail++;
+    }
+  }
+
+  return { finalScore, pass, fail };
 }
-const result = resultReport([98, 87, 67, 91, 92, 33, 87]);
+
+const result = resultReport([]);
 console.log(result);
